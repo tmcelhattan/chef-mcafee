62e03b27279040698f98de2e9ea6d8ae551db87f
root <root@ip-172-31-11-121.us-east-2.compute.internal>
2017-10-04 22:36:45 +0000

    modded

---
+++ mcafee/recipes/install_isectp.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mcafee
+# Recipe:: install_isectp
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
---
+++ mcafee/recipes/install_mcafee.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mcafee
+# Recipe:: install_mcafee
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
